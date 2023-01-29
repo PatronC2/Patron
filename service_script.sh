e7736322fb546b817823551991711825e0798591
Jarvis <mikeyjar1@gmail.com>
2023-01-28 22:39:20 -0500

    define env

--- service_script.sh
+++ service_script.sh
@@ -2,6 +2,7 @@
 # Script to be ran by systemd, please do not manually run this script
 : ${SERVER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )}
 
-/$SERVER_DIR/build/server >> /var/log/patron/server.log 2>&1 & disown
-/$SERVER_DIR/build/webserver >> /var/log/patron/webserver.log 2>&1 & disown
-cd /$SERVER_DIR/Web/client && npm start >> /var/log/patron/webclient.log 2>&1 & disown
+source $SERVER_DIR/.env
+$SERVER_DIR/build/server >> /var/log/patron/server.log 2>&1 & disown
+$SERVER_DIR/build/webserver >> /var/log/patron/webserver.log 2>&1 & disown
+cd $SERVER_DIR/Web/client && npm start >> /var/log/patron/webclient.log 2>&1 & disown
