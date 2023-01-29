4f9476e603787f5ad0f7cb7a78dfc19cb3cb3c27
Jarvis <mikeyjar1@gmail.com>
2023-01-28 23:09:32 -0500

    use go to run everything

--- service_script.sh
+++ service_script.sh
@@ -3,7 +3,9 @@
 : ${SERVER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )}
 
 cd $SERVER_DIR
-./build/server >> /var/log/patron/server.log 2>&1 & disown
-./build/webserver >> /var/log/patron/webserver.log 2>&1 & disown
+/usr/bin/go run server/server.go >> /var/log/patron/server.log 2>&1 & disown
+sleep 10
+/usr/bin/go run Web/server/webserver.go >> /var/log/patron/webserver.log 2>&1 & disown
+sleep 5
 cd $SERVER_DIR/Web/client
 /usr/bin/npm start >> /var/log/patron/webclient.log 2>&1 & disown
