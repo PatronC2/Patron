6f3552936d996ae9db5b01180d41349f641671b2
Jarvis <mikeyjar1@gmail.com>
2024-05-26 23:27:35 -0400

    Get patron using new API

--- Web/client/src/actions/c2actions.jsx
+++ Web/client/src/actions/c2actions.jsx
@@ -2,16 +2,51 @@ import axios from 'axios';
 const C2_ENDPOINT = process.env.REACT_APP_NGINX_IP ? `http://${process.env.REACT_APP_NGINX_IP}:${process.env.REACT_APP_NGINX_PORT}` : `http://${process.env.REACT_APP_WEBSERVER_IP}:${process.env.REACT_APP_WEBSERVER_PORT}`;
 export { C2_ENDPOINT };
 
-export var getCallbacks = async () => {
-
-    const request = await axios.get(`${C2_ENDPOINT}/api/agents`, { withCredentials: true })
-        .then(response => response.data);
-    console.log(request)
-    return {
-        payload: request
+export const login = async () => {
+    try {
+      // Make a POST request to the login endpoint with username and password
+      const response = await axios.post(`${C2_ENDPOINT}/login`, { "username": "patron", "password": "password1!" });
+      
+      // Extract the API token from the response
+      const { token } = response.data;
+  
+      // Store the API token in localStorage or sessionStorage
+      localStorage.setItem('authToken', token);
+  
+      // Return the API token
+      return token;
+    } catch (error) {
+      // Handle login errors
+      console.error('Error logging in:', error);
+      throw error; // Rethrow error to be handled by the caller
     }
+  };
 
-}
+export const getCallbacks = async () => {
+    try {
+        // Fetch authentication token from localStorage or sessionStorage
+        const authToken = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
+ 
+        // Set authentication headers
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`, // Assuming your backend expects Bearer token authentication
+        };
+  
+        // Make authenticated request
+        const response = await axios.get(`${C2_ENDPOINT}/api/agents`, { headers });
+      
+        // Handle successful response
+        console.log(response.data);
+        return {
+            payload: response.data,
+        };
+        } catch (error) {
+        // Handle errors
+        console.error('Error fetching callbacks:', error);
+        throw error; // Rethrow error to be handled by the caller
+    }
+  };
 
 export var getIps = async () => {
 
--- api/api.go
+++ api/api.go
@@ -5,16 +5,17 @@ import (
     "path/filepath"
     "os"
     "strings"
+    "fmt"
 
-    "github.com/PatronC2/Patron/api/server"
+    "github.com/PatronC2/Patron/api/api"
     "github.com/PatronC2/Patron/lib/logger"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
-    server.OpenDatabase()
-    server.InitDatabase()
-    server.CreateAdminUser()
+    api.OpenDatabase()
+    api.InitDatabase()
+    api.CreateAdminUser()
     
     gin.SetMode(gin.ReleaseMode)
     r := gin.Default()
@@ -25,29 +26,29 @@ func main() {
     FileServer(r, "/files", filesDir)
 
     // handle logins
-    r.POST("/login", server.LoginHandler)
+    r.POST("/login", api.LoginHandler)
 
     readRoles := []string{"admin", "operator", "readOnly"}
     writeRoles := []string{"admin", "operator"}
     adminRoles := []string{"admin"}
 
     // Admin functions
-    r.POST("/api/admin/users", server.Auth(adminRoles), server.CreateUserHandler)
-    r.DELETE("/api/admin/users/:username", server.Auth(adminRoles), server.DeleteUserByUsernameHandler)
+    r.POST("/api/admin/users", api.Auth(adminRoles), api.CreateUserHandler)
+    r.DELETE("/api/admin/users/:username", api.Auth(adminRoles), api.DeleteUserByUsernameHandler)
 
     // POST / DELETE requests to non-admin areas use Auth(writeRoles)
-    r.POST("/api/updateagent/:agt", server.Auth(writeRoles), server.UpdateAgentHandler)
-    r.GET("/api/deleteagent/:agt", server.Auth(writeRoles), server.KillAgentHandler)
-    r.POST("/api/payload", server.Auth(writeRoles), server.CreatePayloadHandler)
+    r.POST("/api/updateagent/:agt", api.Auth(writeRoles), api.UpdateAgentHandler)
+    r.GET("/api/deleteagent/:agt", api.Auth(writeRoles), api.KillAgentHandler)
+    r.POST("/api/payload", api.Auth(writeRoles), api.CreatePayloadHandler)
 
     // GET requests to non-admin areas use Auth(readRoles)
-    r.GET("/api/agents", server.Auth(readRoles), server.GetAgentsHandler)
-    r.GET("/api/groupagents", server.Auth(readRoles), server.GetGroupAgents)
-    r.GET("/api/groupagents/:ip", server.Auth(readRoles), server.GetGroupAgentsByIP)
-    r.GET("/api/oneagent/:agt", server.Auth(readRoles), server.GetOneAgentByUUID)
-    r.GET("/api/agent/:agt", server.Auth(readRoles), server.GetAgentByUUID)
-    r.GET("/api/keylog/:agt", server.Auth(readRoles), server.GetKeylogHandler)
-    r.GET("/api/payloads", server.Auth(readRoles), server.GetPayloadsHandler)
+    r.GET("/api/agents", api.Auth(readRoles), api.GetAgentsHandler)
+    r.GET("/api/groupagents", api.Auth(readRoles), api.GetGroupAgents)
+    r.GET("/api/groupagents/:ip", api.Auth(readRoles), api.GetGroupAgentsByIP)
+    r.GET("/api/oneagent/:agt", api.Auth(readRoles), api.GetOneAgentByUUID)
+    r.GET("/api/agent/:agt", api.Auth(readRoles), api.GetAgentByUUID)
+    r.GET("/api/keylog/:agt", api.Auth(readRoles), api.GetKeylogHandler)
+    r.GET("/api/payloads", api.Auth(readRoles), api.GetPayloadsHandler)
 
 
     // Replace with your paths to the certificate and key files
