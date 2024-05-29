fd0bd587c773c7eea215242967c6b64ed0203a4a
Jarvis <mikeyjar1@gmail.com>
2024-05-28 23:01:06 -0400

    use dotenv for ui

--- ui/src/api/axios.jsx
+++ ui/src/api/axios.jsx
@@ -4,7 +4,7 @@ const API_ENDPOINT = `http://${process.env.APP_API_HOST}:${process.env.APP_API_P
 
 const instance = axios.create({
     // I have no idea why using the .env does not work
-    baseURL: `http://192.168.50.32:8000`
+    baseURL: `${API_ENDPOINT}`
 });
 
 export default instance;
