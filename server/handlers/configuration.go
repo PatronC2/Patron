ac82d3f43d342e3c02a54e42b6b2db668ce8c2ef
Jarvis <mikeyjar1@gmail.com>
2024-11-09 17:49:42 -0500

    Split main and handlers

---
+++ server/handlers/configuration.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"net"
+
+	"github.com/PatronC2/Patron/data"
+	"github.com/PatronC2/Patron/types"
+	"github.com/PatronC2/Patron/lib/logger"
+)
+
+// All agents use this handler on their first request
+
+func validateOrCreateAgent(c types.ConfigurationRequest) (types.ConfigurationResponse, bool) {
+	fetch, err := data.FetchOneAgent(c.AgentID)
+	if err != nil {
+		logger.Logf(logger.Warning, "Couldn't fetch agent: %v\n", err)
+		return types.ConfigurationResponse{}, false
+	}
+
+	if fetch.AgentID == "" && c.MasterKey == "MASTERKEY" {
+		data.CreateAgent(c.AgentID, c.ServerIP, c.ServerPort, c.CallbackFrequency, c.CallbackJitter, c.AgentIP, c.Username, c.Hostname)
+		data.CreateKeys(c.AgentID)
+		fetch, err = data.FetchOneAgent(c.AgentID)
+		if err != nil {
+			logger.Logf(logger.Warning, "Couldn't fetch agent after creation: %v\n", err)
+			return types.ConfigurationResponse{}, false
+		}
+	}
+
+	response := types.ConfigurationResponse{
+		ServerIP:         fetch.ServerIP,
+		ServerPort:       fetch.ServerPort,
+		CallbackFrequency: fetch.CallbackFrequency,
+		CallbackJitter:   fetch.CallbackJitter,
+	}
+
+	return response, fetch.AgentID == c.AgentID
+}
+
+type ConfigurationHandler struct{}
+
+func (h *ConfigurationHandler) Handle(request types.Request, conn net.Conn) types.Response {
+    configReq, ok := request.Payload.(types.ConfigurationRequest)
+    if !ok {
+        return types.Response{
+            Type:    types.ConfigurationResponseType,
+            Payload: types.ConfigurationResponse{},
+        }
+    }
+
+    configResponse, success := validateOrCreateAgent(configReq)
+    if !success {
+        return types.Response{
+            Type:    types.ConfigurationResponseType,
+            Payload: types.ConfigurationResponse{},
+        }
+    }
+
+	err := data.UpdateAgentCheckIn(configReq.AgentID)
+	if err != nil {
+		logger.Logf(logger.Error, "Could not update last callback for %v, %v", configReq.AgentID, err)
+	}
+
+    return types.Response{
+        Type:    types.ConfigurationResponseType,
+        Payload: configResponse,
+    }
+}
--- server/server.go
+++ server/server.go
@@ -8,66 +8,10 @@ import (
 	"os"
 
 	"github.com/PatronC2/Patron/data"
+	"github.com/PatronC2/Patron/server/handlers"
 	"github.com/PatronC2/Patron/types"
 	"github.com/PatronC2/Patron/lib/logger"
 )
-
-func validateOrCreateAgent(c types.ConfigurationRequest) (types.ConfigurationResponse, bool) {
-	fetch, err := data.FetchOneAgent(c.AgentID)
-	if err != nil {
-		logger.Logf(logger.Warning, "Couldn't fetch agent: %v\n", err)
-		return types.ConfigurationResponse{}, false
-	}
-
-	if fetch.AgentID == "" && c.MasterKey == "MASTERKEY" {
-		data.CreateAgent(c.AgentID, c.ServerIP, c.ServerPort, c.CallbackFrequency, c.CallbackJitter, c.AgentIP, c.Username, c.Hostname)
-		data.CreateKeys(c.AgentID)
-		fetch, err = data.FetchOneAgent(c.AgentID)
-		if err != nil {
-			logger.Logf(logger.Warning, "Couldn't fetch agent after creation: %v\n", err)
-			return types.ConfigurationResponse{}, false
-		}
-	}
-
-	response := types.ConfigurationResponse{
-		ServerIP:         fetch.ServerIP,
-		ServerPort:       fetch.ServerPort,
-		CallbackFrequency: fetch.CallbackFrequency,
-		CallbackJitter:   fetch.CallbackJitter,
-	}
-
-	return response, fetch.AgentID == c.AgentID
-}
-
-type ConfigurationHandler struct{}
-
-func (h *ConfigurationHandler) Handle(request types.Request, conn net.Conn) types.Response {
-    configReq, ok := request.Payload.(types.ConfigurationRequest)
-    if !ok {
-        return types.Response{
-            Type:    types.ConfigurationResponseType,
-            Payload: types.ConfigurationResponse{},
-        }
-    }
-
-    configResponse, success := validateOrCreateAgent(configReq)
-    if !success {
-        return types.Response{
-            Type:    types.ConfigurationResponseType,
-            Payload: types.ConfigurationResponse{},
-        }
-    }
-
-	err := data.UpdateAgentCheckIn(configReq.AgentID)
-	if err != nil {
-		logger.Logf(logger.Error, "Could not update last callback for %v, %v", configReq.AgentID, err)
-	}
-
-    return types.Response{
-        Type:    types.ConfigurationResponseType,
-        Payload: configResponse,
-    }
-}
 
 
 func (s *Server) handleConnection(conn net.Conn) {
@@ -120,10 +64,14 @@ func (s *Server) Start() {
 	}
 }
 
+// When new functionalities are added, add the new types and handlers to the lists in here
 func NewServer() *Server {
+	gob.Register(types.Request{})
+    gob.Register(types.ConfigurationRequest{})
+    gob.Register(types.ConfigurationResponse{})
     return &Server{
         handlers: map[types.RequestType]Handler{
-            types.ConfigurationRequestType: &ConfigurationHandler{},
+            types.ConfigurationRequestType: &handlers.ConfigurationHandler{},
         },
     }
 }
@@ -138,10 +86,6 @@ func Init() {
 
 	data.OpenDatabase()
 	data.InitDatabase()
-
-	gob.Register(types.Request{})
-    gob.Register(types.ConfigurationRequest{})
-    gob.Register(types.ConfigurationResponse{})
 }
 
 type Handler interface {
