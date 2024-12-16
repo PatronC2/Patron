41b3e89c3fbb15024c81a476f26f8b04aa3bd3d5
Jarvis <mikeyjar1@gmail.com>
2024-12-15 20:52:32 -0500

    change tables

--- data/data.go
+++ data/data.go
@@ -45,43 +45,43 @@ func InitDatabase() {
 
 	AgentSQL := `
 	CREATE TABLE IF NOT EXISTS "agents" (
-		"AgentID" SERIAL PRIMARY KEY,
-		"UUID" TEXT NOT NULL UNIQUE,
-		"ServerIP" TEXT NOT NULL DEFAULT 'Unknown',
-		"ServerPort" TEXT NOT NULL DEFAULT 'Unknown',
-		"CallBackFreq" TEXT NOT NULL DEFAULT 'Unknown',
-		"CallBackJitter" TEXT NOT NULL DEFAULT 'Unknown',
-		"Ip" TEXT NOT NULL DEFAULT 'Unknown',
-		"User" TEXT NOT NULL DEFAULT 'Unknown',
-		"Hostname" TEXT NOT NULL DEFAULT 'Unknown',
-		"OSType" TEXT NOT NULL DEFAULT 'Unknown',
-		"OSArch" TEXT NOT NULL DEFAULT 'Unknown',
-		"OSBuild" TEXT NOT NULL DEFAULT 'Unkown',
-		"CPUS" TEXT NOT NULL DEFAULT 'Unknown',
-		"MEMORY" TEXT NOT NULL DEFAULT 'Unknown',
-		"LastCallBack" TIMESTAMP
+		"agent_id" SERIAL PRIMARY KEY,
+		"uuid" TEXT NOT NULL UNIQUE,
+		"server_ip" TEXT NOT NULL DEFAULT 'Unknown',
+		"server_port" TEXT NOT NULL DEFAULT 'Unknown',
+		"call_back_freq" TEXT NOT NULL DEFAULT 'Unknown',
+		"call_back_jitter" TEXT NOT NULL DEFAULT 'Unknown',
+		"ip" TEXT NOT NULL DEFAULT 'Unknown',
+		"user" TEXT NOT NULL DEFAULT 'Unknown',
+		"hostname" TEXT NOT NULL DEFAULT 'Unknown',
+		"os_type" TEXT NOT NULL DEFAULT 'Unknown',
+		"os_arch" TEXT NOT NULL DEFAULT 'Unknown',
+		"os_build" TEXT NOT NULL DEFAULT 'Unkown',
+		"cpus" TEXT NOT NULL DEFAULT 'Unknown',
+		"memory" TEXT NOT NULL DEFAULT 'Unknown',
+		"last_call_back" TIMESTAMP
 	);
 	CREATE OR REPLACE VIEW agents_status AS
 	SELECT 
-		"AgentID",
-		"UUID",
-		"ServerIP",
-		"ServerPort",
-		"CallBackFreq",
-		"CallBackJitter",
-		"Ip",
-		"User",
-		"Hostname",
-		"OSType",
-		"OSArch",
-		"OSBuild",
-		"CPUS",
-		"MEMORY",
-		"LastCallBack",
+		"agent_id",
+		"uuid",
+		"server_ip",
+		"server_port",
+		"call_back_freq",
+		"call_back_jitter",
+		"ip",
+		"user",
+		"hostname",
+		"os_type",
+		"os_arch",
+		"os_build",
+		"cpus",
+		"memory",
+		"last_call_back",
 		CASE 
-			WHEN "LastCallBack" IS NULL OR "LastCallBack" < NOW() - INTERVAL '1 second' * 2 * CAST("CallBackFreq" AS INTEGER) THEN 'Offline'
+			WHEN "last_call_back" IS NULL OR "last_call_back" < NOW() - INTERVAL '1 second' * 2 * CAST("call_back_freq" AS INTEGER) THEN 'Offline'
 			ELSE 'Online'
-		END AS "Status"
+		END AS "status"
 	FROM 
 		"agents";
 	`
@@ -92,15 +92,15 @@ func InitDatabase() {
 	logger.Logf(logger.Info, "agents table initialized\n")
 
 	CommandSQL := `
-	CREATE TABLE IF NOT EXISTS "Commands" (
-		"CommandID" SERIAL PRIMARY KEY,
-		"UUID" TEXT,
-		"Result" TEXT,
-		"CommandType" TEXT,
-		"Command" TEXT,
-		"CommandUUID" TEXT,
-		"Output" TEXT DEFAULT 'Pending',
-		FOREIGN KEY ("UUID") REFERENCES "agents" ("UUID")
+	CREATE TABLE IF NOT EXISTS "commands" (
+		"command_id" SERIAL PRIMARY KEY,
+		"uuid" TEXT,
+		"result" TEXT,
+		"command_type" TEXT,
+		"command" TEXT,
+		"command_uuid" TEXT,
+		"output" TEXT DEFAULT 'Pending',
+		FOREIGN KEY ("uuid") REFERENCES "agents" ("uuid")
 	);
 	`
 	_, err = db.Exec(CommandSQL)
@@ -111,13 +111,13 @@ func InitDatabase() {
 
 	FilesSQL := `
 	CREATE TABLE IF NOT EXISTS "files" (
-		"FileID" SERIAL PRIMARY KEY,
-		"UUID" TEXT,
-		"Type" TEXT,
-		"Path" TEXT,
-		"Content" BYTEA,
-		"Status" TEXT DEFAULT 'Pending',
-		FOREIGN KEY ("UUID") REFERENCES "agents" ("UUID")
+		"file_id" SERIAL PRIMARY KEY,
+		"uuid" TEXT,
+		"type" TEXT,
+		"path" TEXT,
+		"content" BYTEA,
+		"status" TEXT DEFAULT 'Pending',
+		FOREIGN KEY ("uuid") REFERENCES "agents" ("uuid")
 	);
 	`
 	_, err = db.Exec(FilesSQL)
@@ -127,8 +127,8 @@ func InitDatabase() {
 	logger.Logf(logger.Info, "Files table initialized\n")
 
 	KeylogSQL := `
-	CREATE TABLE IF NOT EXISTS "Keylog" (
-		"KeylogID" SERIAL PRIMARY KEY,
+	CREATE TABLE IF NOT EXISTS "keylog" (
+		"keylogID" SERIAL PRIMARY KEY,
 		"UUID" TEXT,
 		"Keys" TEXT,
 		FOREIGN KEY ("UUID") REFERENCES "agents" ("UUID")
@@ -141,17 +141,17 @@ func InitDatabase() {
 	logger.Logf(logger.Info, "Keylog table initialized\n")
 
 	PayloadSQL := `
-	CREATE TABLE IF NOT EXISTS "Payloads" (
-		"PayloadID" SERIAL PRIMARY KEY,
-		"UUID" TEXT,
-		"Name" TEXT,
-		"Description" TEXT,
-		"ServerIP" TEXT,
-		"ServerPort" TEXT,
-		"CallbackFrequency" TEXT,
-		"CallbackJitter" TEXT,
-		"Concat" TEXT,
-		"isDeleted" INTEGER NOT NULL DEFAULT 0
+	CREATE TABLE IF NOT EXISTS "payloads" (
+		"payloadID" SERIAL PRIMARY KEY,
+		"uuid" TEXT,
+		"name" TEXT,
+		"description" TEXT,
+		"server_ip" TEXT,
+		"server_port" TEXT,
+		"callback_frequency" TEXT,
+		"callback_jitter" TEXT,
+		"doncat" TEXT,
+		"is_deleted" INTEGER NOT NULL DEFAULT 0
 	);
 	`
 	_, err = db.Exec(PayloadSQL)
@@ -176,11 +176,11 @@ func InitDatabase() {
 
 	NotesSQL := `
 	CREATE TABLE IF NOT EXISTS "notes" (
-		"NoteID" SERIAL PRIMARY KEY,
-		"UUID" TEXT NOT NULL,
-		"Note" TEXT,
-		FOREIGN KEY ("UUID") REFERENCES "agents" ("UUID"),
-		UNIQUE ("UUID")
+		"note_id" SERIAL PRIMARY KEY,
+		"uuid" TEXT NOT NULL,
+		"note" TEXT,
+		FOREIGN KEY ("uuid") REFERENCES "agents" ("uuid"),
+		UNIQUE ("uuid")
 	);
 	`
 	_, err = db.Exec(NotesSQL)
@@ -191,12 +191,12 @@ func InitDatabase() {
 
 	TagsSQL := `
 	CREATE TABLE IF NOT EXISTS "tags" (
-		"TagID" SERIAL PRIMARY KEY,
-		"UUID" TEXT NOT NULL,
-		"Key" TEXT NOT NULL,
-		"Value" TEXT,
-		FOREIGN KEY ("UUID") REFERENCES "agents" ("UUID"),
-		UNIQUE ("UUID", "Key")
+		"tag_id" SERIAL PRIMARY KEY,
+		"uuid" TEXT NOT NULL,
+		"key" TEXT NOT NULL,
+		"value" TEXT,
+		FOREIGN KEY ("uuid") REFERENCES "agents" ("uuid"),
+		UNIQUE ("uuid", "key")
 	);
 	`
 	_, err = db.Exec(TagsSQL)
@@ -207,25 +207,25 @@ func InitDatabase() {
 
 	RedirectorsSQL := `
 	CREATE TABLE IF NOT EXISTS "redirectors" (
-		"RedirectorID" TEXT PRIMARY KEY,
-		"Name" TEXT NOT NULL,
-		"Description" TEXT,
-		"ForwardIP" TEXT,
-		"ForwardPort" TEXT,
-		"ListenPort" TEXT NOT NULL,
-		"LastReport" TIMESTAMP
+		"redirector_id" TEXT PRIMARY KEY,
+		"name" TEXT NOT NULL,
+		"description" TEXT,
+		"forward_ip" TEXT,
+		"forward_port" TEXT,
+		"listen_port" TEXT NOT NULL,
+		"last_report" TIMESTAMP
 	);
 	CREATE OR REPLACE VIEW redirector_status AS
 	SELECT 
-		"RedirectorID",
-		"Name",
-		"Description",
-		"ForwardIP",
-		"ForwardPort",
-		"ListenPort",
-		"LastReport",
+		"redirector_id",
+		"name",
+		"description",
+		"forward_ip",
+		"forward_port",
+		"listen_port",
+		"last_report",
 		CASE 
-			WHEN "LastReport" IS NULL OR "LastReport" < NOW() - INTERVAL '10 minutes' THEN 'Offline'
+			WHEN "last_report" IS NULL OR "last_report" < NOW() - INTERVAL '10 minutes' THEN 'Offline'
 			ELSE 'Online'
 		END AS "Status"
 	FROM 
@@ -238,27 +238,28 @@ func InitDatabase() {
 	logger.Logf(logger.Info, "Redirectors table initialized\n")
 
 	EventsSQL := `
-	CREATE TABLE IF NOT EXISTS "events" (
-		"EventID" SERIAL PRIMARY KEY,
-		"Name" TEXT NOT NULL,
-		"Description" TEXT,
-		"Script" BYTEA,
-		"Schedule" TEXT NOT NULL,
-		"LastRun" TIMESTAMP
+	CREATE TABLE IF NOT EXISTS events (
+		event_id SERIAL PRIMARY KEY,
+		name TEXT NOT NULL,
+		description TEXT,
+		script BYTEA,
+		schedule TEXT NOT NULL,
+		status TEXT NOT NULL CHECK (Status IN ('NOTRUN', 'RUNNABLE', 'RUNNING', 'COMPLETE')) DEFAULT 'UNKNOWN',
+		lastrun TIMESTAMP
 	);
 	`
 	_, err = db.Exec(EventsSQL)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	logger.Logf(logger.Info, "Eventss table initialized")
+	logger.Logf(logger.Info, "Events table initialized")
 
 	ActionsSQL := `
 	CREATE TABLE IF NOT EXISTS "actions" (
-		"ActionID" SERIAL PRIMARY KEY,
-		"Name" TEXT NOT NULL,
-		"Description" TEXT,
-		"File" BYTEA NOT NULL
+		"action_id" SERIAL PRIMARY KEY,
+		"name" TEXT NOT NULL,
+		"description" TEXT,
+		"file" BYTEA NOT NULL
 	);
 	`
 	_, err = db.Exec(ActionsSQL)
@@ -269,9 +270,9 @@ func InitDatabase() {
 
 	TriggersSQL := `
 	CREATE TABLE IF NOT EXISTS "triggers" (
-		"ID" SERIAL PRIMARY KEY,
-		"EventID" INT REFERENCES events("EventID") ON DELETE CASCADE,
-		"ActionID" INT REFERENCES actions("ActionID") ON DELETE CASCADE
+		"id" SERIAL PRIMARY KEY,
+		"event_id" INT REFERENCES events("event_id") ON DELETE CASCADE,
+		"action_id" INT REFERENCES actions("action_id") ON DELETE CASCADE
 	);
 	`
 	_, err = db.Exec(TriggersSQL)
