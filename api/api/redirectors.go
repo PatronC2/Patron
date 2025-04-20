14bf050c42223c0b1461a34510882fc27c757ac3
Jarvis <mikeyjar1@gmail.com>
2025-04-20 17:06:25 -0400

    Use a template for the install script

--- api/api/redirectors.go
+++ api/api/redirectors.go
@@ -1,7 +1,8 @@
 package api
 
 import (
-	"fmt"
+	"bytes"
+	"html/template"
 	"net/http"
 	"os"
 	"os/exec"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/PatronC2/Patron/data"
 	"github.com/PatronC2/Patron/lib/logger"
+	"github.com/PatronC2/Patron/types"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
@@ -47,100 +49,41 @@ func CreateRedirectorHandler(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ListenPort"})
 	} else {
 		commandString := "docker save -o /app/payloads/redirector.tar patronc2/redirector"
-		script := fmt.Sprintf(`#!/bin/bash -xe
+		tmpl, err := template.ParseFiles("templates/redirector_install.sh.tmpl")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to load template"})
+			return
+		}
 
-linking_key="%s"
-api_ip="%s"
-api_port="%s"
-# This is the port set when the app was compiled. Do not change.
-redirector_port=%s
-# This port can be freely changed to change the port that the redirector listens on.
-external_redirector_port=%s
-tar_file="redirector.tar"
+		tplData := types.RedirectorTemplateData{
+			LinkingKey:     newRedirectorID,
+			ApiIP:          api_ip,
+			ApiPort:        api_port,
+			RedirectorPort: redirector_port,
+			ExternalPort:   body["ListenPort"],
+			ForwardIP:      body["ForwardIP"],
+			ForwardPort:    body["ForwardPort"],
+		}
 
-rm -f $tar_file
+		var scriptBuf bytes.Buffer
+		if err := tmpl.Execute(&scriptBuf, tplData); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to render script"})
+			return
+		}
 
-docker=$(which docker || echo "not found")
-wget=$(which wget || echo "not found")
-
-if [ -x "$docker" ]; then
-    echo "Docker Check OK"
-else
-	# Remove conflicting Docker packages
-	for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-	apt-get remove -y $pkg
-	done
-
-	# Install Docker
-	apt update
-	apt install -y ca-certificates curl
-	install -m 0755 -d /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-	chmod a+r /etc/apt/keyrings/docker.asc
-
-	echo \
-	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-	tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-	apt-get update
-	apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-	systemctl enable --now docker
-	docker --version
-fi
-
-daemon_file="/etc/docker/daemon.json"
-desired_config='{"ipv6": true, "fixed-cidr-v6": "2001:db8:1::/64"}'
-
-if [ -f "$daemon_file" ]; then
-    current_config=$(jq -c . < "$daemon_file" 2>/dev/null || echo "{}")
-    normalized_desired_config=$(echo "$desired_config" | jq -c .)
-
-    if [ "$current_config" != "$normalized_desired_config" ]; then
-        echo "Updating Docker daemon.json..."
-        echo "$normalized_desired_config" > "$daemon_file"
-        systemctl restart docker
-    else
-        echo "Docker daemon.json already configured as desired. Skipping update and restart."
-    fi
-else
-    echo "Creating Docker daemon.json with desired configuration..."
-    echo "$desired_config" > "$daemon_file"
-    systemctl restart docker
-fi
-
-if [ -x "$wget" ]; then
-    echo "wget Check OK"
-else
-	apt install -y wget
-fi
-
-wget --no-check-certificate https://$api_ip:$api_port/fileserver/$tar_file
-docker load -i $tar_file
-docker run -d \
-	-p $external_redirector_port:$redirector_port \
-	-e MAIN_SERVER_IP="%s" \
-	-e MAIN_SERVER_PORT="%s" \
-	-e FORWARDER_PORT="$redirector_port" \
-	-e LINKING_KEY="$linking_key" \
-	-e API_IP="$api_ip" \
-	-e API_PORT="$api_port" \
-	-v ./logs:/app/logs \
-	patronc2/redirector
-`, newRedirectorID, api_ip, api_port, redirector_port, body["ListenPort"], body["ForwardIP"], body["ForwardPort"])
+		script := scriptBuf.Bytes()
 
 		logger.Logf(logger.Info, "Running command: %s", commandString)
 		cmd := exec.Command("sh", "-c", commandString)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Internal Server Error", "details": err.Error()})
 		} else {
 			data.CreateRedirector(newRedirectorID, body["Name"], body["Description"], body["ForwardIP"], body["ForwardPort"], body["ListenPort"])
 			c.Header("Content-Disposition", "attachment; filename=redirector_install.sh")
-			c.Data(http.StatusOK, "application/x-sh", []byte(script))
+			c.Data(http.StatusOK, "application/x-sh", script)
 		}
 	}
 }
