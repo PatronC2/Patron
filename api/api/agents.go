6f527a6f716bed843a2f6fd82a2b0d5a1717b959
Michael Jarvis <jarvismichael@protonmail.com>
2026-04-24 21:05:48 -0400

    remove deprecated api endpoints (#111)

--- api/api/agents.go
+++ api/api/agents.go
@@ -12,17 +12,6 @@ import (
 	"github.com/google/uuid"
 )
 
-func GetAgentsHandler(c *gin.Context) {
-	// Get all agents
-	agents, err := data.Agents()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get agents"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"data": agents})
-}
-
 func FilterAgentsHandler(c *gin.Context) {
 	filters := make(map[string]string)
 
